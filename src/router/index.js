96f7f211fbee1f1f43b30792cde1b034ce797b27
hsy <274917321@qq.com>
2020-10-19 15:28:21 +0800

    ssss

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Router from 'vue-router'
 
 import login from '@/components/login.vue'
 import register from '@/components/register.vue'
-import trade from '@/components/trade.vue'
 import publish from '@/components/publish.vue'
 
 Vue.use(Router)
@@ -12,7 +11,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
 		{
-		  path: '/login',
+		  path: '/',
 		  name: 'login',
 		  component: login
 		},
@@ -20,11 +19,6 @@ export default new Router({
 			path: '/register',
 			name: 'register',
 			component: register
-		},
-		{
-			path: '/trade',
-			name: 'trade',
-			component: trade
 		},
 		{
 			path: '/publish',
